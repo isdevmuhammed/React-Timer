0bf53b419e9299c12c42471c665c0a71132ad81a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 11:44:23 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ function App() {
   const [userInput, setUserInput] = useState("")
   const [randomInput, setRandomInput] = useState('')
 
-  const fib = useCallback((n) => {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-  }, [])
-
-  const fibNumber = useMemo(() => fib(userInput), [userInput, fib]);
-
-  const myArray = useMemo(() => getArray(), [])
-
-  useEffect(() => {
-    console.log('New Array');
-  }, [myArray])
 
   return (
     <main className="App">
