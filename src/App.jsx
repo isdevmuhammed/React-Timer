5f2cd3108cc1e27d36bd1ea28613080e40fd705d
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 12:50:19 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,11 @@ function App() {
     }
 
     const resetTimer = () => {
-        
+        stopTimer();
+        if(seconds) {
+            renders.current = 0;
+            setSeconds(0)
+        }
         inputRef.current.focus();
     }
 
