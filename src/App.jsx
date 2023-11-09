630d1e21bab770866641f3a0d5f293da49bcdba8
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 14:11:53 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,82 @@
 import { useRef, useState } from "react";
 
 function App() {
-  const [randomInput, setRandomInput] = useState("");
-  const [seconds, setSeconds] = useState(0);
+    const [randomInput, setRandomInput] = useState("");
+    const [seconds, setSeconds] = useState(0);
 
-  const renders = useRef(0);
+    const renders = useRef(0);
 
-  const inputRef = useRef();
+    const inputRef = useRef();
 
-  const timerId = useRef();
+    const timerId = useRef();
 
-  const startTimer = () => {
-    timerId.current = setInterval(() => {
-      renders.current++;
+    const startTimer = () => {
+        timerId.current = setInterval(() => {
+            renders.current++;
 
-      setSeconds((prev) => prev + 1);
-    }, 1000);
-    inputRef.current.focus();
-  };
+            setSeconds((prev) => prev + 1);
+        }, 1000);
+        inputRef.current.focus();
+    };
 
-  const stopTimer = () => {
-    clearInterval(timerId.current);
-    timerId.current = 0;
+    const stopTimer = () => {
+        clearInterval(timerId.current);
+        timerId.current = 0;
 
-    inputRef.current.focus();
-  };
+        inputRef.current.focus();
+    };
 
-  const resetTimer = () => {
-    stopTimer();
-    if (seconds) {
-      renders.current++;
-      setSeconds(0);
-    }
-    inputRef.current.focus();
-  };
+    const resetTimer = () => {
+        stopTimer();
+        if (seconds) {
+            renders.current++;
+            setSeconds(0);
+        }
+        inputRef.current.focus();
+    };
 
-  const handleInputChange = (e) => {
-    setRandomInput(e.target.value);
+    const handleInputChange = (e) => {
+        setRandomInput(e.target.value);
 
-    renders.current++;
-  };
+        renders.current++;
+    };
 
-  // const focusOnInput = () => {
-  //     inputRef.current.focus()
-  // }
+    // const focusOnInput = () => {
+    //     inputRef.current.focus()
+    // }
 
-  return (
-    <main className="App">
-      <input
-        type="text"
-        ref={inputRef}
-        required
-        value={randomInput}
-        placeholder="type anything"
-        onChange={handleInputChange}
-      />
-      <p>
-        Renders: <code>{renders.current || "..."}</code>
-      </p>
-      <br />
-      <br />
-      <section>
-        {/* <button onClick={focusOnInput}>Focus</button> */}
-        <button onClick={stopTimer}>Stop</button>
-        <button onClick={startTimer}>Start</button>
-      </section>
-      <button onClick={resetTimer}>Reset</button>
-      <br />
-      <br />
+    return (
+        <main className="App">
+            <input
+                type="text"
+                ref={inputRef}
+                required
+                value={randomInput}
+                placeholder="type anything"
+                onChange={handleInputChange}
+            />
+            <p>
+                Renders: <code>{renders.current || "..."}</code>
+            </p>
+            <br />
+            <br />
+            <section>
+                {/* <button onClick={focusOnInput}>Focus</button> */}
+                <button onClick={stopTimer}>Stop</button>
+                <button onClick={startTimer}>Start</button>
+            </section>
+            <button onClick={resetTimer}>Reset</button>
+            <br />
+            <br />
 
-      <p>Seconds: {seconds}</p>
+            <p>Seconds: {seconds}</p>
 
-      <br />
-      <br />
+            <br />
+            <br />
 
-      <p>{randomInput}</p>
-    </main>
-  );
+            <p>{randomInput}</p>
+        </main>
+    );
 }
 
 export default App;
